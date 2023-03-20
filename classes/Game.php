b5b9325fe5847f30780a30d3424027aeae93b9d0
Nouster <djebali.mohamed88@gmail.com>
2023-03-20 14:51:01 +0100

    define game class, add an object in db

--- classes/Game.php
+++ classes/Game.php
@@ -1,5 +1,75 @@
 <?php
 
 class Game {
+    private int $id;
+    private string $name;
+    private string $picture;
+    private string $description;
+    private DateTime $releaseGame;
+    private DateTime $startGame;
+    private bool $getting;
+
+    public function __construct(int $id, string $name, string $picture, string $description, DateTime $releaseGame, DateTime $startGame, bool $getting) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->picture = $picture;
+        $this->description = $description;
+        $this->releaseGame = $releaseGame;
+        $this->startGame = $startGame;
+        $this->getting = $getting;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getPicture(): string {
+        return $this->picture;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function getReleaseGame(): DateTime {
     
+        return $this->releaseGame;
+    }
+
+    public function getStartGame(): DateTime {
+        return $this->startGame;
+    }
+
+    public function getIsGetting(): bool {
+        return $this->getting;
+    }
+
+
+    public function setName(string $name) {
+        $this->name = $name;
+    }
+
+    public function setPicture(string $picture) {
+        $this->picture = $picture;
+    }
+
+    public function setDescription(string $description) {
+        $this->description = $description;
+    }
+
+    public function setReleaseGame(DateTime $releaseGame) {
+        $this->releaseGame = $releaseGame;
+    }
+
+    public function setStartGame(DateTime $startGame) {
+        $this->startGame = $startGame;
+    }
+
+    public function setGetting(bool $getting) {
+        $this->getting = $getting;
+    }
 }
--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php require_once 'layout/header.php'; 
 require_once 'db/pdo.php';
-$stmt = $pdo->query("INSERT INTO test VALUES (1, 'mohamed');");
+require_once 'classes/Game.php';
+// $stmt = $pdo->query("INSERT INTO test VALUES (1, 'mohamed');");
+// $pdo->query("CREATE TABLE test(id int, name VARCHAR(50));");
+// $pdo->query("INSERT INTO test VALUES (1, 'mohamed');");
+
+// $stmt = $pdo->prepare("SELECT * FROM test;");
+// $stmt->execute();
+$date = new DateTime();
+$game = new Game(2, "Fifa 22", "assets/img/fifa_22.jpeg", "Un jeu de foot très populaire", $date, $date, true);
+
+$stmt = $pdo->prepare("INSERT INTO game VALUES (?,?,?,?,?,?,?)");
+$stmt->execute([
+    $game->getId(),
+    $game->getName(),
+    $game->getPicture(),
+    $game->getDescription(),
+    $game->getReleaseGame()->format('Y-m-d'),
+    $game->getStartGame()->format('Y-m-d'),
+    $game->getIsGetting()
+]);
+
+
 
 
 
